bfa831ac0b0464aa4809d339e623f62bf9e5bd52
tahakh03 <khuwaja.taha@mail.utoronto.ca>
2023-11-29 12:40:08 -0500

    m

--- FSM/control.v
+++ FSM/control.v
@@ -6,78 +6,71 @@ input straight,
 input right,
 input left,
 input oneframe, 
-input done_bg,
-input done_car, 
-input done_race,
-input done_erase
-output reg start_race, 
+input [7:0] counterx, countery,
 output reg draw_bg, 
 output reg draw_car, 
-output reg drive,
-output reg resetsignal,
-output reg clear
+output reg erase,
+output reg plot,
 )
 reg [3:0] current_state, next_state;
 
-localparam 
-START_RACE = 4'd0,
+localparam START_RACE = 4'd0, 
 DRAW_BG = 4'd1,
 DRAW_CAR = 4'd2,
 WAIT_MOVE = 4'd3,
 STRAIGHT = 4'd4,
 LEFT = 4'd5,
-WAIT_LEFT = 4'd6,
-RIGHT = 4'd7,
-WAIT_RIGHT = 4'd8,
-ERASE_CAR = 4'd9,
-SET_RESET_SIGNAL = 4'd10;
+RIGHT = 4'd6,
+ERASE_CAR = 4'd7,
+UPDATE_CAR = 4'd8;
 
 always@ (*)
 begin
     case (current_state)
 
     START_RACE: next_state = DRAW_BG;
-    DRAW_BG: next_state = done_bg ? DRAW_CAR : DRAW_BG;
+    DRAW_BG:
+    begin
+        if (counterx == 8'd160)
+            next_state = DRAW_CAR;
+        else
+            next_state = DRAW_BG;
+    end
     DRAW_CAR:
     begin
-        if (done_car)
-        begin
-            if (!start) next_state = SET_RESET_SIGNAL;
-            else if (straight == 1'b1 && oneframe) next_state = ERASE_CAR;
-            else if (left == 1'b1) next_state = WAIT_LEFT;
-            else if (right == 1'b1) next_state = WAIT_RIGHT;
-            else next_state = WAIT_MOVE;
-        end
-        else next_state = DRAW_CAR;
+        if (counterx == 8'd4)
+            next_state = WAIT_MOVE;
+        else
+            next_state = DRAW_CAR;
     end
     WAIT_MOVE:
     begin
-        if (forward == 1'b1 && oneframe) next_state = ERASE_CAR;
-        else if (left == 1'b1 || right == 1'b1) next_state = ERASE_CAR;
-        else next_state = WAIT_MOVE;
+        if (straight) 
+            next_state = STRAIGHT;
+        else if (left)
+            next_state = LEFT;
+        else if (right)
+            next_state = RIGHT;
+        else
+            next_state = WAIT_MOVE;
     end
-    STRAIGHT: next_state = DRAW_CAR;
+    STRAIGHT: next_state = ERASE_CAR;
 
-    LEFT: next_state = DRAW_CAR;
+    LEFT: next_state = ERASE_CAR;
 
-    WAIT_LEFT: next_state = left ? WAIT_LEFT : WAIT_MOVE;
-
-    RIGHT = next_state = DRAW_CAR;
-
-    WAIT_RIGHT = next_state = right ? WAIT_RIGHT : WAIT_MOVE;
+    RIGHT = next_state = ERASE_CAR_CAR;
 
     ERASE_CAR:
     begin
-        if (done_erase)
-        begin
-            if (forward == 1'b1) next_state = STRAIGHT;
-            else if (left == 1'b1) next_state = LEFT;
-            else if (right == 1'b1) next_state = RIGHT;
-            else next_state = DRAW_CAR;
-        end
-        else next_state = ERASE_CAR;
+        if (counterx == 8'd160)
+            next_state = UPDATE_CAR
     end
-    SET_RESET_SIGNAL: next_state = draw_menu;
+    UPDATE_CAR:
+    begin
+        if (counterx == 8'd4)
+            next_state = WAIT_MOVE;
+    end
+    default: next_state = START_RACE;
 
     endcase
 end
@@ -85,31 +78,29 @@ end
 endmodule
 
 always @(*)
-begin
-    start_race = 1'b0; 
+begin 
     draw_bg = 1'b0; 
     draw_car = 1'b0; 
-    drive = 1'b0;
     resetsignal = 1'b0;
-    clear = 1'b0;
+    erase = 1'b0;
 
 case (current_state)
     DRAW_BG: 
     begin
         draw_bg = 1'b1;
+        plot = 1'b1;
+
     end
     DRAW_CAR:
     begin
-        if (done_car)
-            draw_car = 1'b0;
-        else
-            draw_car = 1'b1;
+        draw_car = 1'b1;
+        plot = 1'b1;
     end
     ERASE_CAR:
     begin
-        if (clear)
+        erase = 1'b1;
+        plot = 1'b1;
     end
-    SET_RESET_SIGNAL = resetsignal = 1'b1;
 endcase
 end
 
--- FSM/datapath.v
+++ FSM/datapath.v
@@ -1,3 +1,46 @@
-module datapath ()
+module datapath (
+input clock, reset,
+input draw_bg, draw_car, drive, resetsignal, erase, plot
+input color,
+output reg [7:0] xcounter, ycounter, xout, yout,
+)
+reg [7:0] xpoint, ypoint;
+
+always @(posedge clock)
+begin
+    if (!reset)
+        xout <= 8'b0;
+        yout <= 8'b0;
+        counterx <= 8'b0;
+        countery <= 8'b0;
+    else
+    if (plot)
+      begin
+         if (inc)
+         begin
+            countery <= countery + 1'b1;
+            counterx <= 8'b0;
+         end
+         else
+            counterx <= counterx + 1'b1;
+      end
+      if (done_car || done_erase || done_bg)
+      begin
+         countery <= 8'b0;
+         counterx <= 8'b0;
+      end
+end
+
+always @(*)
+begin
+    if (draw_bg || erase)
+        xout <= xpoint + counterx;
+        yout <= ypoint + countery;
+        // color
+    if (draw_car)
+        xout <= xpoint + counterx;
+        yout <= ypoint + countery;
+        //color
+end
 
 endmodule
