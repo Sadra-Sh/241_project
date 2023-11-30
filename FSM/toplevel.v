5b03c1dfbd96c7d1a0a52b8d5477dc6e8efcf781
tahakh03 <khuwaja.taha@mail.utoronto.ca>
2023-11-29 19:39:56 -0500

    Update toplevel.v

--- FSM/toplevel.v
+++ FSM/toplevel.v
@@ -1,12 +1,12 @@
-module toplevel(iResetn, iClock, oX, oY, oColour, oPlot);
+module toplevel(iResetn, iClock, oX, oY, oColour, start);
    parameter X_SCREEN_PIXELS = 8'd160;
    parameter Y_SCREEN_PIXELS = 7'd120;
 
    input wire iClock;
    output wire [7:0] oX; // VGA pixel coordinates
-   output wire [6:0] oY;
+   output wire [7:0] oY;
 
    output wire [2:0] oColour; // VGA pixel colour (0-7)
    output wire oPlot; // Pixel draw enable
-   
+
 endmodule
